c5c0fd89e5c49b3a461fa4dce294759d1f588b04
Beck8077 <52420458+Beck8077@users.noreply.github.com>
2025-04-01 10:21:31 +0500

    Update lesson4hw.py

--- lesson-4/homework/lesson4hw.py
+++ lesson-4/homework/lesson4hw.py
@@ -7,12 +7,12 @@
 # Write a Python script to add a key to a dictionary.
 
 # Students = {
-#     "Alice": 85,
-#     "Bob": 78,
-#     "Charlie": 90,
-#     "David": 62,
-#     "Eve": 45
-# }
+    "Alice": 85,
+    "Bob": 78,
+    "Charlie": 90,
+    "David": 62,
+    "Eve": 45
+}
 
 # Students['Bekzod'] = 100
 # print(Students)
@@ -20,51 +20,51 @@
 # 3. Concatenate Multiple Dictionaries
 # Write a Python script to concatenate the following dictionaries to create a new one.
 
-# dic1 = {1: 10, 2: 20}
-# dic2 = {3: 30, 4: 40}
-# dic3 = {5: 50, 6: 60}
-# dic4 = {}
-# dic4.update(dic1)
-# dic4.update(dic2)
-# dic4.update(dic3)
-# print(dic4)
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+dic4 = {}
+dic4.update(dic1)
+dic4.update(dic2)
+dic4.update(dic3)
+print(dic4)
 
 
 # 4. Generate a Dictionary with Squares
 # Write a Python script to generate and print a dictionary that contains a number (between 1 and n) in the form (x, x*x).
 
-# numbers = list(range(1,5))
-# new_dict = {number:number**2 for number in numbers}
-# print(new_dict)
+numbers = list(range(1,5))
+new_dict = {number:number**2 for number in numbers}
+print(new_dict)
 
 # 5. Dictionary of Squares (1 to 15)
 # Write a Python script to print a dictionary where the keys are numbers between 1 and 15 (both included) and the values are the square of the keys.
 
-# numbers = list(range(1,16))
-# new_dict = {number:number**2 for number in numbers}
-# print(new_dict)
+numbers = list(range(1,16))
+new_dict = {number:number**2 for number in numbers}
+print(new_dict)
 
 # Set Exercises
 # 1. Create a Set
 # Write a Python program to create a set.
 
-# nums = set(range(5, 50, 3))
-# print(nums)
+nums = set(range(5, 50, 3))
+print(nums)
 
 # 2. Iterate Over a Set
 # Write a Python program to iterate over sets.
 
-# nums = set(range(5, 50, 3))
+nums = set(range(5, 50, 3))
 
-# for num in nums:
-#     print(num)
+for num in nums:
+    print(num)
 
 
 # 3. Add Member(s) to a Set
 # Write a Python program to add member(s) to a set.
 
-# nums = set(range(5, 50, 3))
-# nums.add(51)
+nums = set(range(5, 50, 3))
+nums.add(51)
 
 # 4. Remove Item(s) from a Set
 # Write a Python program to remove item(s) from a given set.
